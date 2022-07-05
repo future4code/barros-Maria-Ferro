7a66fcd9e73860d31f9e69ecefc0360359806dd2
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-07-05 13:35:42 -0300

    Adicionar resolução Aula Redenderização de Listas 1

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import Post from './components/Post/Post';
-import './style.css'
+import './style.js'
+import { MainContainer } from './style.js';
 
 
 function App() {
-return(
-  <div className='MainContainer'>
-           <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
 
-            <Post
-            nomeUsuario={'maria'}
-            fotoUsuario={'https://picsum.photos/id/1013/50/50'}
-            fotoPost={'https://picsum.photos/id/237/200/150'}
-          />
+const listaPost = [
+  {nomeUsuario: 'paulinha',
+  fotoUsuario: 'https://picsum.photos/50/50',
+  fotoPost: 'https://picsum.photos/200/150'},
 
-            <Post
-            nomeUsuario={'matheus'}
-            fotoUsuario={'https://picsum.photos/id/777/50/50'}
-            fotoPost={'https://picsum.photos/id/12/200/150'}
-          />
-  </div>
-)
+  {nomeUsuario: 'maria',
+  fotoUsuario: 'https://picsum.photos/id/1013/50/50',
+  fotoPost: 'https://picsum.photos/id/237/200/150'},
 
-}
+  {nomeUsuario: 'matheus',
+  fotoUsuario: 'https://picsum.photos/id/777/50/50',
+  fotoPost: 'https://picsum.photos/id/12/200/150'},
+]
 
+const componentePost = listaPost.map((valor, index) => {
+  return <li key={index}><Post nomeUsuario={valor.nomeUsuario} fotoUsuario={valor.fotoUsuario} fotoPost={valor.fotoPost}/></li>
+})
+
+console.log(listaPost)
+
+return (
+  <MainContainer>
+    <ul>{componentePost}</ul>
+  </MainContainer>
+)}
 
 export default App;
---
+++ modulo3/estados-insta4/insta4/src/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+
+    ul {
+        list-style: none;
+    }
+
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center 
+`
