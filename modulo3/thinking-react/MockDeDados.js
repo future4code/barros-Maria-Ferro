9edb952342564b60a646fd8c5676fa05f15c6f8a
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-07-14 18:51:48 -0300

    Adicionar exercício Thinking in React

---
+++ modulo3/thinking-react/MockDeDados.js
@@ -0,0 +1,47 @@
+let botãoAdicionar = <button>Adicionar Produto</button>
+
+const listaDeProdutos = [
+    {
+        id: 1,
+        nome: "Spoink de Pelúcia",
+        valor: 65,
+        imagem: "https://m.media-amazon.com/images/I/61lm1+MI4zL._AC_SX522_.jpg",
+        adicionar: {botãoAdicionar}
+    },
+    {
+        id: 2,
+        nome: "Pikachu de Pelúcia",
+        valor: 75,
+        imagem: "https://images-americanas.b2w.io/produtos/3144563494/imagens/pokemon-pikachu-pelucia-animal-recheado-com-flor-8-idade-2/3144563494_1_large.jpg",
+        adicionar: {botãoAdicionar}
+    },
+    {
+        id: 3,
+        nome: "Bulbasaur de Pelúcia",
+        valor: 68,
+        imagem: "https://http2.mlstatic.com/D_NQ_NP_632562-CBT50471348049_062022-O.webp",
+        adicionar: {botãoAdicionar}
+    },
+    {
+        id: 4,
+        nome: "Charmander de Pelúcia",
+        valor: 78,
+        imagem: "https://cdn.shopify.com/s/files/1/0582/6455/1588/products/pokemon-pelucia-original-p70-pimpolinho-20-25cm-charmander-862386_800x.jpg?v=1657240310",
+        adicionar: {botãoAdicionar}
+    },
+    {
+        id: 5,
+        nome: "Squirtle de Pelúcia",
+        valor: 75,
+        imagem: "https://cdn.shopify.com/s/files/1/0582/6455/1588/products/pokemon-pelucia-original-p70-pimpolinho-20-25cm-squirtle-100281_800x.jpg?v=1657240310",
+        adicionar: {botãoAdicionar}
+    },
+    {
+        id: 6,
+        nome: "JigglyPuff de Pelúcia",
+        valor: 58,
+        imagem: "https://ae01.alicdn.com/kf/Sdac056e2c3bb4d1db945f29cc2884d68Q/5-p-s-lote-16cm-Jigglypuff-Plush-Toys-Bonito-Pokemon-Jigglypuff-Pel-cia-Macia-Bichos-de.jpg_Q90.jpg_.webp",
+        adicionar: {botãoAdicionar}
+    },
+
+]
---
+++ modulo3/thinking-react/README.md
@@ -0,0 +1,11 @@
+# THINKING IN REACT
+
+## Imagem Árvore de Componentes
+
+! [Árvore de Componentes] (Árvore.png)
+
+## Imagem Árvore de Componentes - onde está o Estado
+
+! [Árvore de Componentes - com Estado] (Árvore-useState.png)
+
+As partes em vermelho são onde devem ter estado.
