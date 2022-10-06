70b8d204af50c7a3768afd1c56c193b26c0431ba
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-10-06 11:32:33 -0300

    Resolução Exercício 4

--- modulo5/revisao-javascript-backend/backend/exercicio2/index.js
+++ modulo5/revisao-javascript-backend/backend/exercicio2/index.js
@@ -18,12 +18,12 @@ const adicionarClientes = (id, nome) => {
         let idExistente = false
 
         for (let i = 0; i < clientes.length; i++) {
-        if (clientes[i].id === id) {
-            idExistente = true
-        }
+            if (clientes[i].id === id) {
+                idExistente = true
+            }
         }
 
-        if(idExistente) {
+        if (idExistente) {
             return `Erro. Parâmetro inválido: '${id}'. Id já existente.`
         } else {
             clientes.push( {id: id, nome: nome} )
@@ -47,20 +47,20 @@ const gerarTabuada = (num) => {
         return `Erro. Parâmetro inválido: '${num}'. (Deve ser entre 1 e 10)."`
     } else {
 
-    tabuada = [
-        `${num} x 0 = ${num * 0}`,
-        `${num} x 1 = ${num * 1}`, 
-        `${num} x 2 = ${num * 2}`,
-        `${num} x 3 = ${num * 3}`,
-        `${num} x 4 = ${num * 4}`,
-        `${num} x 5 = ${num * 5}`,
-        `${num} x 6 = ${num * 6}`,
-        `${num} x 7 = ${num * 7}`,
-        `${num} x 8 = ${num * 8}`,
-        `${num} x 9 = ${num * 9}`,
-        `${num} x 10 = ${num * 10}`]
+        tabuada = [
+            `${num} x 0 = ${num * 0}`,
+            `${num} x 1 = ${num * 1}`, 
+            `${num} x 2 = ${num * 2}`,
+            `${num} x 3 = ${num * 3}`,
+            `${num} x 4 = ${num * 4}`,
+            `${num} x 5 = ${num * 5}`,
+            `${num} x 6 = ${num * 6}`,
+            `${num} x 7 = ${num * 7}`,
+            `${num} x 8 = ${num * 8}`,
+            `${num} x 9 = ${num * 9}`,
+            `${num} x 10 = ${num * 10}`]
 
-    return tabuada
+        return tabuada
     }
 }
 
--- modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
+++ modulo5/revisao-javascript-backend/backend/exercicio4/exercicio.js
@@ -1,1 +1,51 @@
-console.log("exercicio 4");
+const primeiraLista = [
+	{
+		nome: "Banana"
+	},
+	{
+		nome: "Laranja"
+	}, 
+    {
+        nome: "Morango"
+    },
+    {
+        nome: "Abacate"
+    }
+]
+
+const segundaLista= [
+	{
+		nome: "Laranja"
+	},
+	{
+		nome: "Cebola"
+	},
+    {
+        nome: "Cenoura"
+    },
+    {
+        nome: "Morango"
+    },
+    {
+        nome: "Mamão"
+    }
+]
+
+const geraItensUnicos = (lista1, lista2) => {
+
+    const novaLista = [...lista1, ...lista2]
+
+     for (let i = 0; i < novaLista.length; i++) {
+        let check = novaLista[i].nome
+        for (let y = i+1; y < novaLista.length; y++) {
+            if (check === novaLista[y].nome) {
+                novaLista.splice(y,1)
+                y--
+            }
+        }
+    }
+
+    return novaLista
+}
+
+console.log(geraItensUnicos(primeiraLista, segundaLista))
