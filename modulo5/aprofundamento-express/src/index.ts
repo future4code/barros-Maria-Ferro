987e24d1fa84a0b1d919296de69d50273978ed19
Maria Fernandez de Moura Ferro <mariafmferro@gmail.com>
2022-10-27 15:55:38 -0300

    Fiz o exercício 10 e tentei fazer o 11

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,7 +1,10 @@
 import express, {Request, Response} from 'express'
 import cors from 'cors'
 import { toDoList } from './data'
+import { ToDo } from './types'
 
+
+const fs = require('fs');
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -14,7 +17,15 @@ app.get("/ping", (req: Request, res: Response) => {
 
 // exercício 02 no arquivo types.ts
 
-// exercício 03 no arquivo data.ts
+// exercício 03: array no arquivo data.ts
+
+fs.readFile('/Users/maria/Desktop/Repositórios/barros-Maria-Ferro/modulo5/aprofundamento-express/src/data.ts', 'utf8', (err: any, data: any) => {
+    if (err) {
+        console.error(err)
+        return;
+    }
+    console.log(data)
+})
 
 // exercício 04
 
@@ -59,6 +70,11 @@ app.post("/todos", (req: Request, res: Response) => {
             title: title,
             completed: completed
         })
+/*         fs.writeFile('/Users/maria/Desktop/Repositórios/barros-Maria-Ferro/modulo5/aprofundamento-express/src/data.ts', uptadedToDoList, (err: any) => {
+            if (err) {
+                console.log(err)
+            }
+        }) */
     }
 
     res.status(200).send(uptadedToDoList)
@@ -127,6 +143,39 @@ app.get("/todos/:user", (req: Request, res: Response) => {
 })
 
 // exercício 09: link da documentação no PR
+
+// exercício 10
+
+type ToDoNew = {
+        selectedUser: ToDo[],
+        others: ToDo[]
+}
+
+app.get("/todos/:user", (req: Request, res: Response) => {
+    const userId = req.params.user
+    const findUser = toDoList.find(user => user.userId === userId)
+
+    if (!userId) {
+        res.status(400).send("Adicione o id de usuário.")
+    } else if (!findUser) {
+        res.status(400).send("Não há usuários com o id informado.")
+    } else {
+        const filteredByUser = toDoList.filter((user) =>{
+            return user.userId === userId
+        })
+
+        const other = toDoList.filter((user) => {
+            return user.userId !== userId
+        })
+
+        const toDoNew: ToDoNew = {
+            selectedUser: filteredByUser,
+            others: other,
+        }
+
+        res.status(200).send(toDoNew)
+    }
+})
 
 app.listen(3003, () => {
     console.log("Server running on http://localhost:3003/")
